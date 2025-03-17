52e64d64b17194c25748c45a965a92c7b88b80f4
Nikita B <n2h9z4@gmail.com>
2025-03-17 21:02:42 +0100

    feature: 11869: mvp: apply k8s yamls
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

---
+++ install.sh
@@ -0,0 +1,7 @@
+kubectl krew uninstall meshery-cluster-snapshot 2>/dev/null
+
+go build -o bin/kubectl-meshery-cluster-snapshot cmd/main/*.go
+
+tar -czvf bin/v0.0.1.tar.gz bin/kubectl-meshery-cluster-snapshot
+
+sha256sum bin/v0.0.1.tar.gz
