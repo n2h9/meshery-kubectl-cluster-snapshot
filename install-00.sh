7d87575c152652feb0e2ace11860842199bb02a6
Nikita B <n2h9z4@gmail.com>
2025-03-22 15:15:52 +0100

    feature: 11869: mvp: rename wrap-os to os
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

--- install-00.sh
+++ install-00.sh
@@ -2,7 +2,7 @@
 
 kubectl krew uninstall meshery-cluster-snapshot 2>/dev/null
 
-go build -o bin/kubectl-meshery-cluster-snapshot cmd/wrap-os/*.go
+go build -o bin/kubectl-meshery-cluster-snapshot cmd/os/*.go
 
 tar -czvf bin/v0.0.1.tar.gz bin/kubectl-meshery-cluster-snapshot
 
